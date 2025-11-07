d57550f16db9ed67e00ba0f5bd7b5aed405eeb65
Jethro Browell <32899312+jbrowell@users.noreply.github.com>
2025-11-07 08:33:55 +0000

    Corrections and improvement
    Correct file read and add option to real all files in directory

--- R/load_uktradeinfo_data.R
+++ R/load_uktradeinfo_data.R
@@ -2,39 +2,57 @@
 #'
 #' Load a single bulk data file from UK Trade Info
 #'
-#' @param path Path to the file to load
+#' @param path Path to a file to read, or directory containing multiple files,
+#' subdirectories are recursed.
 #'
 #' @return A `data.table` of trade data.
 #'
 #' @export
-load_uktradeinfo <- function(path) {
+read_uktradeinfo <- function(path) {
 
-  BDSimp <- data.table::fread(path, header = F, strip.white = F)
+  if( file_test("-f", path) ) {
 
-  BDSimp <- data.table(
-    transform(BDSimp,
-              period_reference=substr(V1,1,6),
-              type=substr(V1,7,7),
-              month_of_account=substr(V1,8,13),
-              comcode=substr(V1,14,21),
-              SITC=substr(V1,22,26),
-              COD_SEQUENCE=substr(V1,27,29),
-              COD_ALPHA=substr(V1,30,31),
-              port_code=substr(V1,32,34),
-              PORT_ALPHA=substr(V1,35,37),
-              country_of_origin=substr(V2,1,3),
-              COO_ALPHA=substr(V2,4,5),
-              mode_of_transport=substr(V2,6,7),
-              statistical_value=substr(V2,8,9),
-              net_mass=substr(V2,10,21),
-              supplementary_unit=substr(V2,22,33),
-              suppression=substr(V2,34,34),
-              flow=substr(V2,35,37),
-              record_type=substr(V2,38,41)))
+    BDS <- data.table::fread(path, header = F, strip.white = F, sep = NULL)
 
-  BDSimp$V1 <- NULL
-  BDSimp$V2 <- NULL
+    BDS <- BDS[,.(PERREF=substr(V1,1,6),
+                  TYPE=substr(V1,7,7),
+                  MONTHAC=substr(V1,8,13),
+                  COMCODE=substr(V1,14,21),
+                  SITC=substr(V1,22,26),
+                  COD_SEQ=substr(V1,27,29),
+                  COD_ALPHA=substr(V1,30,31),
+                  PORT_SEQ=substr(V1,32,34),
+                  PORT_CODE=substr(V1,35,37),
+                  COO_SEQ=substr(V1,38,40),
+                  COO_ALPHA=substr(V1,41,42),
+                  MODE_OF_TRANSPORT=substr(V1,43,44),
+                  STAT_VALUE=substr(V1,45,56),
+                  NET_MASS=substr(V1,57,68),
+                  SUMM_UNIT=substr(V1,69,80),
+                  SUPRESSION=substr(V1,81,81),
+                  FLOW=substr(V1,82,84),
+                  REC_TYPE=substr(V1,85,85))]
 
-  return(BDSimp)
+    return(BDS)
+
+  } else if(file_test("-d", path)) {
+
+    files <- list.files(path,pattern = ".txt",
+                        full.names = T,
+                        recursive = T)
+
+    BDS_all <- data.table()
+
+    for(f in files) {
+
+      BDS_all <- rbind(BDS_all, read_uktradeinfo(f))
+
+    }
+
+    return(BDS_all)
+
+  } else {
+    stop("path is not a file or directory.")
+  }
 
 }
