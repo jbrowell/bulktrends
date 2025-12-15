0c1db552908a752047739aa063d1599f639c83dd
janeetam <janeeta.maunthrooa@glasgow.ac.uk>
2025-12-15 14:33:33 +0000

    New function to utils

--- R/utils.R
+++ R/utils.R
@@ -35,3 +35,46 @@ extract_netmass_ts <- function (import_data,
 
   return(ts_data)
 }
+
+#'
+#' Extract suitable model matrix for a given commodity code
+#'
+#' This function evaluates a set of formulas with trend and seasonal terms
+#' as exogenous variables and selects the ARIMA model with the lowest AICc.
+#'
+#' @param data A time series object containing the dependent variable.
+#'
+#' @return A model matrix of the trend and seasonal regressors of the selected model.
+#'
+#' @export
+identify_best_model <- function (data){
+
+  trend = 1:length(data)
+  month = rep(1:12, length.out = length(data))
+  formulas <- list(~ trend,
+                   ~ sin(2*pi*month/12) + cos(2*pi*month/12),
+                   ~ trend + sin(2*pi*month/12) + cos(2*pi*month/12),
+                   ~ trend + sin(4*pi*month/12) + cos(4*pi*month/12))
+  model<- list()
+  aicc_values <- rep(Inf, length(formulas))
+
+  for (i in seq_along(formulas)) {
+    X <- model.matrix(formulas[[i]], data = data.frame(trend, month))
+    #print(X)
+    model <- try(auto.arima(data,
+                            xreg = X,
+                            max.p = 5,
+                            max.q = 5,
+                            seasonal = FALSE), silent=T)
+
+    if ("try-error" %in% class(model)){
+      cat("Model", i, "failed\n")
+      next}
+
+    model[[i]] <- model
+    aicc_values[i] <- model$aicc
+  }
+  print(aicc_values)
+  best_aic <- which.min(aicc_values)
+  return(model.matrix(formulas[[best_aic]], data = data))
+}
