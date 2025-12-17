d1f0d20a223e5c0cf79a28a83af6631dfb13ebeb
Jethro Browell <jethro.browell@glasgow.ac.uk>
2025-12-17 13:56:16 +0000

    Add required metric argument to example

--- R/detect_anomaly_netmass.R
+++ R/detect_anomaly_netmass.R
@@ -1,31 +1,28 @@
-
-NULL
-
 # Large scale anomaly detection (Work in progress)
 
 chapters <- unique(substr(imports$COMCODE, 1,2))
 all_outliers <- list()
 
 for (i in chapters){
-#create time series
-ts_data <- extract_netmass_ts(imports, i)
+  #create time series
+  ts_data <- extract_netmass_ts(imports, i)
 
-#detect outliers using function identify_best_model() in xreg
-detect_anomaly <- tso(y = log(ts_data),
-                      types = c("AO", "LS", "TC", "IO"),
-                      tsmethod = "auto.arima",
-                      xreg = select_best_model(ts_data))
+  #detect outliers using function identify_best_model() in xreg
+  detect_anomaly <- tso(y = log(ts_data),
+                        types = c("AO", "LS", "TC", "IO"),
+                        tsmethod = "auto.arima",
+                        xreg = select_best_model(ts_data, metric = "aic"))
 
-#visualise results
-plot.tsoutliers(detect_anomaly)
-title(main = paste("Outliers for", i), col.main = "darkblue", cex.main = 1, line = 2.5)
+  #visualise results
+  plot.tsoutliers(detect_anomaly)
+  title(main = paste("Outliers for", i), col.main = "darkblue", cex.main = 1, line = 2.5)
 
-#store outliers data produced
-outliers_dt <- as.data.table(detect_anomaly$outliers)
-outliers_dt[, chapters := i]
-all_outliers[[i]] <- outliers_dt
+  #store outliers data produced
+  outliers_dt <- as.data.table(detect_anomaly$outliers)
+  outliers_dt[, chapters := i]
+  all_outliers[[i]] <- outliers_dt
 
-#flag "volatile" chapters?
+  #flag "volatile" chapters?
 }
 
 # Combine all results into a single data.table
