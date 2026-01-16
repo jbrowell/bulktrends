26feae1360e583c57194b8dcda0976adb351729a
Jethro Browell <jethro.browell@glasgow.ac.uk>
2026-01-16 15:41:02 +0000

    Generalise extract_ts function

--- R/utils.R
+++ R/utils.R
@@ -5,30 +5,30 @@
 #' This function extracts the monthly sum "NET_MASS" from import_data for
 #' any level of hierarchy (HS2, HS4, HS6 and CN8) found in the dataset.
 #'
-#' @param import_data A `data.table` containing trade data. Must include "COMCODE".
+#' @param import_data A `data.table` containing trade data. Must include columns "COMCODE", "month" and `quantity`.
 #' @param code A character string representing any HS2/HS4/HS6/CN8 code.
+#' @param quantity Quantity to be extracted and aggregated as time series, e.g. "NET_MASS" or "STAT_VALUE".
 #'
 #' @return Time series object of NET_MASS
 #'
 #' @export
-extract_netmass_ts <- function (import_data,
-                                code) {
+extract_ts <- function (import_data,
+                                code,
+                                quantity = "NET_MASS") {
 
   all_months <- unique(import_data$month)
-  import_data <- import_data[substr(COMCODE, 1, nchar(code)) == code]
-  #import_data[,COMCODE:=substr(COMCODE,1,nchar(code))]
-  #import_data <- import_data[COMCODE==code]
+  import_data <- copy(import_data[substr(COMCODE, 1, nchar(code)) == code])
 
   missing_months <- all_months[!all_months %in% import_data$month]
   if(length(missing_months) > 0) {
     warning("Missing months detected for code ",code,": ", paste(missing_months))
   }
 
-  import_data[, NET_MASS := as.numeric(NET_MASS)]
-  import_data <-  import_data[, .(NET_MASS = sum(NET_MASS, na.rm = T)), by=month]
+  import_data <-  import_data[, .(temp = sum(get(quantity), na.rm = T)), by=month]
+  setnames(import_data,"temp",quantity)
 
   first_month <- import_data[, min(month)]
-  ts_data <- ts(import_data$NET_MASS, start = c(year(first_month), month(first_month)), frequency = 12)
+  ts_data <- ts(import_data[[quantity]], start = c(year(first_month), month(first_month)), frequency = 12)
 
   return(ts_data)
 }
