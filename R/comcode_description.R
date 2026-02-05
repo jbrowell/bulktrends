b198d3e09bddbd0f8f1e328f0bc4972df7dd22a9
Jethro Browell <32899312+jbrowell@users.noreply.github.com>
2026-02-05 13:32:07 +0000

    Run devtools::document()

--- R/comcode_description.R
+++ R/comcode_description.R
@@ -1,23 +1,23 @@
-#' Hierarchichal classification of a given commodity code
-#' 
+#' Hierarchical classification of a given commodity code
+#'
 #' This function displays the information related to a given commodity code and
 #' any of its subcodes (HS2/HS4/HS6/CN8).
-#' 
+#'
 #' @param code A character string representing any HS2/HS4/HS6/CN8 code.
 #' @param lookup_table A `data.table` containing details of each code level.
-#' 
+#'
 #' @return A display of the hierarchical information of code.
-#' 
+#'
 #' @export
 comcode_description <- function(code, lookup_table) {
   lookup_table <- as.data.table(lookup_table)
   meta <- lookup_table[Cn8Code == code | Hs2Code == code | Hs4Code == code | Hs6Code == code]
-  
+
   desc <- list(Hs2 = paste(meta$Hs2Code[1], " — ", meta$Hs2Description[1]),
                Hs4 = paste(meta$Hs4Code[1], " — ", meta$Hs4Description[1]),
                Hs6 = paste(meta$Hs6Code[1], " — ", meta$Hs6Description[1]),
                Cn8 = paste(meta$Cn8Code[1], " — ", meta$Cn8LongDescription[1]))
-  
+
   # structured format
   cat("\nHierarchical Description for Code:", code, "\n")
   cat("---------------------------------------------\n")
