ef07b4c287333e3a7ebc2c0e9a5f3c7d88e34a69
janeetam <janeeta.maunthrooa@glasgow.ac.uk>
2026-04-22 17:43:15 +0100

    Update detect_anomalies.R
    Return list updated. Progress bar and sparse rate added. Verbose still work in progress.

--- R/detect_anomalies.R
+++ R/detect_anomalies.R
@@ -23,14 +23,21 @@ detect_anomalies <- function(
     model_selection_metric = "aic",
     scale_ts = FALSE,
     freq = NULL,
+    verbose = FALSE,
     ...
 ){
 
   all_outliers <- list()
   tso_output <- list()
-
   ts_prep <- list()
+  list_of_ts <- list()
+
+  with_progress({
+  p <- progressor(along = codes)
+
   for (i in seq_along(codes)){
+
+   # if (verbose) message("Starting code:", codes[i])
 
     ts_prep[[i]] <- tryCatch(extract_ts(import_data,
                           code = codes[i],
@@ -42,11 +49,21 @@ detect_anomalies <- function(
 
     if(inherits(ts_prep, "error")){
       message(paste("Skipping code", code, ":", ts_prep))
+      p(sprintf("Skipped %s", code))
       next
     }
   }
-
+  #if (verbose) message("ts_prep for", codes[i])
   process_ts <- function(ts_data, code) {
+
+    sparse_rate <- mean(ts_data[[quantity]] == 0)
+   # n_ts_data <- length(unique(ts_data[[quantity]]))
+
+    if (sparse_rate > 0.4) {
+      message(paste("Skipping code", code, ":", round(sparse_rate * 100,2), "% zeros"))
+      p(sprintf("Skipped %s", code))
+      return(NULL)
+    }
 
     selected_model <- tryCatch(select_best_model(data = ts_data,
                                         response_col = quantity,
@@ -57,8 +74,11 @@ detect_anomalies <- function(
 
     if(inherits(selected_model, "error")){
       message(paste("Skipping code", code, ":", selected_model))
+      p(sprintf("Skipped %s", code))
       return(NULL)
     }
+
+   # if (verbose) message("selected_model for", codes[i])
 
     detect_anomaly <- tryCatch(
       tso(
@@ -75,16 +95,40 @@ detect_anomalies <- function(
 
     if(inherits(detect_anomaly, "error")){
       message(paste("Skipping code", code, ":", detect_anomaly))
+      p(sprintf("Skipped %s", code))
       return(NULL)
     }
+    Sys.sleep(2)
+    p(sprintf("Processing code %s", code))
+    #if (verbose) message("Finished code: ", code)
 
     #storing all tso output
-    tso_entry <- setNames(
-      list(list(tso=detect_anomaly,
-                y=if(scale_ts){as.ts(scale(ts_data[[quantity]]))} else{as.ts(ts_data[[quantity]])},
-                time_index=ts_data[[date_col]])),
-      code
-    )
+   # tso_entry <- setNames(
+    #  list(list(tso=detect_anomaly,
+     #           y=if(scale_ts){as.ts(scale(ts_data[[quantity]]))} else{as.ts(ts_data[[quantity]])},
+      #         time_index=ts_data[[date_col]])),
+      #code
+    #)
+
+    xreg <- detect_anomaly$fit$xreg
+
+    if (!is.null(xreg)) {
+    xreg <- as.matrix(xreg)
+
+    #identify outlier columns
+    outlier_cols <- colnames(xreg)
+    outlier_cols <- outlier_cols[substr(outlier_cols, 1, 2) %in% c("AO", "LS", "TC", "IO")]
+    #ts_table <- data.table(ts_data, xreg[, outlier_cols, drop=F])
+
+     if (length(outlier_cols) > 0) {
+     xreg_outliers <- xreg[, outlier_cols, drop = F]
+    } else {xreg_outliers <- NULL}
+
+    } else {xreg<- NULL}
+
+    ts_entry <-  setNames(list(list(y=if(scale_ts){as.ts(scale(ts_data[[quantity]]))} else{as.ts(ts_data[[quantity]])},
+                     xreg_outliers = xreg_outliers)),
+                     code)
 
     if (nrow(detect_anomaly$outliers)>0){
 
@@ -101,16 +145,19 @@ detect_anomalies <- function(
                                    model_formula = deparse(selected_model$formula))
     }
 
-    list(outliers = outliers_entry, tso = tso_entry)
+    list(outliers = outliers_entry, #tso = tso_entry,
+         list_of_ts = ts_entry)
   }
 
   results <- future.apply::future_mapply(process_ts, ts_prep, codes, SIMPLIFY = FALSE)
   results <- Filter(Negate(is.null), results)
 
   all_outliers <- lapply(results, `[[`, "outliers")
-  tso_output <- do.call(c, lapply(results, `[[`, "tso"))
-
+  list_of_ts <- lapply(results, `[[`, "list_of_ts")
+ # tso_output <- do.call(c, lapply(results, `[[`, "tso"))
+})
   return(list(outliers=rbindlist(all_outliers, fill = TRUE),
-         tso_output=tso_output))
+              list_of_ts = list_of_ts))
+         #tso_output=tso_output),
 
 }
