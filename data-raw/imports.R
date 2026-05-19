a98951539546fa77d58ddb42abb05a256e15ee7e
Jethro Browell <jethro.browell@glasgow.ac.uk>
2026-05-19 10:30:51 +0100

    Add flexibility to specify dates. Also download "latest" files, not only archive.

---
+++ data-raw/imports.R
@@ -0,0 +1,8 @@
+# Download monthly import data from UK Trade Info
+#
+# dest_dir is suggested for developers to enable smooth running of other
+# scripts, including the User Guide.
+
+download_uktradeinfo_bulk(dest_dir = "data/Rbuildignore/imports_hmrc/",
+                          type = "imports",
+                          overwrite = FALSE)
