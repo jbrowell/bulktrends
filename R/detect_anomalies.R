6263d81a25195a5f3b4dda3631fa1a37e0fac674
Jethro Browell <jethro.browell@glasgow.ac.uk>
2026-06-24 12:20:03 +0100

    Suggested pattern for detect_anomalies with tsibble.

--- R/detect_anomalies.R
+++ R/detect_anomalies.R
@@ -25,9 +25,8 @@
 #'
 #' @export
 detect_anomalies <- function(
-  import_data,
-  codes,
-  quantity = "NET_MASS",
+  data,
+  response_col = "NET_MASS",
   date_col = "DATE_START",
   model_selection_metric = AIC,
   scale_ts = FALSE,
@@ -35,142 +34,23 @@ detect_anomalies <- function(
   verbose = FALSE,
   tso_params = list()
 ) {
-  ts_prep <- list()
-  for (code in codes) {
-    ts_data <- tryCatch(
-      extract_ts(
-        import_data,
-        code = code,
-        date_col = date_col,
-        quantity = quantity,
-        fill_missing = 0,
-        freq = freq
-      ),
-      error = function(e) e
-    )
-
-    if (inherits(ts_data, "error")) {
-      message(paste("Skipping code", code, ":", ts_data))
-      next
-    } else {
-      ts_prep[[code]] <- ts_data
-    }
+  if (!inherits(data, "list")) {
+    stop("`data` must be a list.")
   }
 
-  rm(ts_data)
-  codes <- names(ts_prep)
-  p <- progressr::progressor(along = codes)
+  time_series_ids <- names(data)
 
-  process_ts <- function(ts_data, code) {
-    sparse_rate <- mean(ts_data[[quantity]] == 0, na.rm = T)
-
-    if (!is.na(sparse_rate) && sparse_rate > 0.4) {
-      message(paste(
-        "Skipping code",
-        code,
-        ":",
-        round(sparse_rate * 100, 2),
-        "% zeros"
-      ))
-      p(sprintf("Skipped %s", code))
-      return(NULL)
-    }
-
-    if (verbose) {
-      message(sprintf("Running selected_model for %s", code))
-    }
-
-    selected_model <- tryCatch(
-      select_best_model(
-        data = ts_data,
-        response_col = quantity,
-        date_col = date_col,
-        metric = model_selection_metric,
-        break_detection = TRUE
-      ),
-      error = function(e) e
-    )
-
-    if (inherits(selected_model, "error")) {
-      message(paste("Skipping code", code, ":", selected_model))
-      p(sprintf("Skipped %s", code))
-      return(NULL)
-    }
-
-    if (verbose) {
-      message(sprintf("Running detect_anomaly for %s", code))
-    }
-
-    ts_data <- selected_model$data
-    break_entry <- selected_model$break_entry
-    xreg_all <- model.matrix(selected_model$formula, data = ts_data)
-
-    #for ~ -1 models
-    if (ncol(xreg_all) == 0) {
-      xreg_all <- NULL
-    }
-
-    outliers <- detect_outliers(
-      data = ts_data,
-      quantity = "NET_MASS",
-      scale_ts = scale_ts,
-      xreg = xreg_all,
-      tso_params = tso_params
-    )
-
-    ts_data <- outliers$data
-
-    if (!is.null(outliers$outliers) && nrow(outliers$outliers) > 0) {
-      #store tso outliers data produced
-      new_outliers <- as.data.table(outliers$outliers)
-      new_outliers[, code := code]
-      new_outliers[,
-        model_formula := paste(deparse(selected_model$formula), collapse = " ")
-      ]
-
-      new_outliers[, time := ts_data$DATE_START[ind]]
-      new_outliers[, anomaly_type := "Outlier"]
-
-      outliers_entry <- new_outliers
-    } else {
-      outliers_entry <- data.table()
-    }
-
-    #add breaks table
-    if (!is.null(break_entry) && nrow(break_entry) > 0) {
-      break_entry[, code := code]
-      break_entry[,
-        model_formula := paste(deparse(selected_model$formula), collapse = " ")
-      ]
-      break_entry[, anomaly_type := "Break"]
-
-      outliers_entry <- rbindlist(
-        list(outliers_entry, break_entry),
-        fill = TRUE
-      )
-
-      setorder(outliers_entry, time)
-    } else {
-      break_entry <- data.table()
-    }
-
-    #no break no outlier option
-    if (nrow(outliers_entry) == 0) {
-      outliers_entry <- data.table(
-        code = code,
-        model_formula = paste(deparse(selected_model$formula), collapse = " "),
-        anomaly_type = "None"
-      )
-    }
-
-    p(sprintf("Completed code %s", code))
-    return(list(outliers = outliers_entry, list_of_ts = ts_data, code = code))
-  }
+  p <- progressr::progressor(along = time_series_ids)
 
   results <- future.apply::future_mapply(
-    process_ts,
+    .detect_anomalies_single_ts,
     ts_prep,
-    codes,
+    time_series_ids,
+    MoreArgs = list(
+      response_col = response_col,
+      date_col = date_col,
+      scale_ts = scale_ts
+    ),
     SIMPLIFY = FALSE,
     future.globals = list(),
     future.packages = c("bulktrends")
@@ -179,9 +59,140 @@ detect_anomalies <- function(
   results <- Filter(Negate(is.null), results)
   all_outliers <- lapply(results, `[[`, "outliers")
   list_of_ts <- lapply(results, `[[`, "list_of_ts")
-  names(list_of_ts) <- lapply(results, `[[`, "code")
+  names(list_of_ts) <- lapply(results, `[[`, "id")
   return(list(
     outliers = rbindlist(all_outliers, fill = TRUE),
     list_of_ts = list_of_ts
   ))
+}
+
+#' Detect anomalies in a single time series (Internal)
+#' @note This needs to return a list of time series with original tsibble attributes. See suggested pattern in comments...
+#'
+.detect_anomalies_single_ts <- function(
+  ts_data,
+  id,
+  response_col,
+  date_col,
+  scale_ts
+) {
+
+  if ( inherits(ts_data,"tsibble") ) {
+    # Store attributes & convert to data.table...
+    #
+    # .tsibble_attributes <- attr(ts_data)
+    # ts_data <- data.table::as.data.table(ts_data)
+  }
+
+  sparse_rate <- mean(ts_data[[response_col]] == 0, na.rm = T)
+
+  if (!is.na(sparse_rate) && sparse_rate > 0.4) {
+    message(paste(
+      "Skipping time series",
+      id,
+      ":",
+      round(sparse_rate * 100, 2),
+      "% zeros"
+    ))
+    p(sprintf("Skipped %s", id))
+    return(NULL)
+  }
+
+  if (verbose) {
+    message(sprintf("Running selected_model for %s", id))
+  }
+
+  selected_model <- tryCatch(
+    select_best_model(
+      data = ts_data,
+      response_col = response_col,
+      date_col = date_col,
+      metric = model_selection_metric,
+      break_detection = TRUE
+    ),
+    error = function(e) e
+  )
+
+  if (inherits(selected_model, "error")) {
+    message("Skipping time series", id, ":", selected_model)
+    p(sprintf("Skipped %s", id))
+    return(NULL)
+  }
+
+  if (verbose) {
+    message(sprintf("Running detect_anomaly for %s", id))
+  }
+
+  ts_data <- selected_model$data
+  break_entry <- selected_model$break_entry
+  xreg_all <- model.matrix(selected_model$formula, data = ts_data)
+
+  #for ~ -1 models
+  if (ncol(xreg_all) == 0) {
+    xreg_all <- NULL
+  }
+
+  outliers <- detect_outliers(
+    data = ts_data,
+    quantity = response_col, ### Change quantity to response_col!!!
+    scale_ts = scale_ts,
+    xreg = xreg_all,
+    tso_params = tso_params
+  )
+
+  ts_data <- outliers$data
+
+  if (!is.null(outliers$outliers) && nrow(outliers$outliers) > 0) {
+    #store tso outliers data produced
+    new_outliers <- as.data.table(outliers$outliers)
+    new_outliers[, id := id]
+    new_outliers[,
+      model_formula := paste(deparse(selected_model$formula), collapse = " ")
+    ]
+
+    new_outliers[, time := ts_data$DATE_START[ind]]
+    new_outliers[, anomaly_type := "Outlier"]
+
+    outliers_entry <- new_outliers
+  } else {
+    outliers_entry <- data.table()
+  }
+
+  #add breaks table
+  if (!is.null(break_entry) && nrow(break_entry) > 0) {
+    break_entry[, id := id]
+    break_entry[,
+      model_formula := paste(deparse(selected_model$formula), collapse = " ")
+    ]
+    break_entry[, anomaly_type := "Break"]
+
+    outliers_entry <- rbindlist(
+      list(outliers_entry, break_entry),
+      fill = TRUE
+    )
+
+    setorder(outliers_entry, time)
+  } else {
+    break_entry <- data.table()
+  }
+
+  #no break no outlier option
+  if (nrow(outliers_entry) == 0) {
+    outliers_entry <- data.table(
+      id = id,
+      model_formula = paste(deparse(selected_model$formula), collapse = " "),
+      anomaly_type = "None"
+    )
+  }
+
+  if ( inherits(ts_data,"tsibble") ) {
+    # Covert back to tsibble and assign attributes...
+    #
+    # ts_data <- fable::as.tsibble(ts_data)
+    # attr(ts_data) <- .tsibble_attributes
+
+  }
+
+  p(sprintf("Completed time series %s", id))
+  return(list(outliers = outliers_entry, list_of_ts = ts_data, id = id))
 }
