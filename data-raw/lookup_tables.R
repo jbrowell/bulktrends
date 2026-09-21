c7d1de3b0be782b932b5c5782280adcd5f360f5a
Jethro Browell <32899312+jbrowell@users.noreply.github.com>
2026-09-21 13:56:28 +0100

    Add created_at attribute to dataset
    Record the date the script was run as an attribute on the dataset.

--- data-raw/lookup_tables.R
+++ data-raw/lookup_tables.R
@@ -2,5 +2,8 @@
 
 ## Download
 
+## Record the date this script was run as an attribute on the dataset
+attr(..., "created_at") <- as.character(Sys.Date())
+
 ## Add to package
 usethis::use_data(...)
