c8ac65343886589b31c3cffc07d291bc2170a85b
Gabriel Dantas <biel.dantasrolim@gmail.com>
2026-09-24 12:41:27 +0100

    Creating data for test-comcode_description

--- data-raw/lookup_tables.R
+++ data-raw/lookup_tables.R
@@ -4,12 +4,12 @@
 raw_table <- uktrades_request(endpoint = "Commodity")$value
 
 ## Filter the data
-lookup_table <- raw_table[
-  raw_table$Hs2Code == "01" & raw_table$Hs4Code == "0101" & raw_table$Hs6Code == "010129",
+test_comcode_table <- raw_table[
+  raw_table$Hs2Code == "01",
 ]
 
 ## Record the date this script was run as an attribute on the dataset
-attr(lookup_table, "created_at") <- as.character(Sys.Date())
+attr(test_comcode_table, "created_at") <- as.character(Sys.Date())
 
 ## Add to package
-usethis::use_data(lookup_table, overwrite = TRUE)
+usethis::use_data(test_comcode_table, overwrite = TRUE)
