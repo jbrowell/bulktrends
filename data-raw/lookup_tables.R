d3aade82757809e0cd35782ce2d3efcf7f69566d
Gabriel Dantas <biel.dantasrolim@gmail.com>
2026-09-24 13:18:39 +0100

    Resolving conflict on lookup_tables.R

--- data-raw/lookup_tables.R
+++ data-raw/lookup_tables.R
@@ -1,7 +1,6 @@
 # Download and update look-up tables in package data
 
 ## Download
-<<<<<<< HEAD
 raw_table <- uktrades_request(endpoint = "Commodity")$value
 
 ## Filter the data
@@ -14,11 +13,3 @@ attr(test_comcode_table, "created_at") <- as.character(Sys.Date())
 
 ## Add to package
 usethis::use_data(test_comcode_table, overwrite = TRUE)
-=======
-
-## Record the date this script was run as an attribute on the dataset
-attr(..., "created_at") <- as.character(Sys.Date())
-
-## Add to package
-usethis::use_data(...)
->>>>>>> origin/feature/59-first-unit-tests
